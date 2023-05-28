d134cbcb704b6a8656ac6c41da6fd903490954e7
bmsk <vv99911@gmail.com>
2023-05-28 11:37:09 +0900

    SettingUpdate

--- app/src/main/java/org/cazait/ui/MainActivity.kt
+++ app/src/main/java/org/cazait/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package org.cazait
+package org.cazait.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.cazait.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
