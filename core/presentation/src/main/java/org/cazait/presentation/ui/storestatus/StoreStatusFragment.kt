9bd141020be97ba70257f2847bc88d49ff084cf2
bmsk <vv99911@gmail.com>
2023-07-04 10:52:19 +0900

    Feat: 카페 상태 변경 화면 구성

--- core/presentation/src/main/java/org/cazait/presentation/ui/storestatus/StoreStatusFragment.kt
+++ core/presentation/src/main/java/org/cazait/presentation/ui/storestatus/StoreStatusFragment.kt
@@ -1,7 +1,38 @@
 package org.cazait.presentation.ui.storestatus
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import org.cazait.presentation.R
+import org.cazait.presentation.databinding.FragmentStoreStatusBinding
 
-class StoreStatusFragment: Fragment() {
+@AndroidEntryPoint
+class StoreStatusFragment : Fragment() {
+    private var _binding: FragmentStoreStatusBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: StoreStatusViewModel by viewModels()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_store_status,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
---
+++ core/presentation/src/main/java/org/cazait/presentation/ui/storestatus/StoreStatusViewModel.kt
@@ -0,0 +1,12 @@
+package org.cazait.presentation.ui.storestatus
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StoreStatusViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
