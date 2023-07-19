9fc500f3a89874f9cbde4c833247ca7fbaec949b
Jaeeepp <101336601+Jaeeepp@users.noreply.github.com>
2023-07-19 19:17:36 +0900

    [Add]#14-카페 정보 수정, 공지사항 UI 추가
    - 카페 정보 수정 UI추가
    - 카페 공지사항 UI 추가
    - nav 설정까지 완료

---
+++ core/presentation/src/main/kotlin/org/cazait/presentation/ui/description/CafeDescriptionViewModel.kt
@@ -0,0 +1,11 @@
+package org.cazait.presentation.ui.description
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CafeDescriptionViewModel @Inject constructor(
+
+): ViewModel(){
+}
