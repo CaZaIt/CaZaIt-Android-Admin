bdf2b13358fb2d7df5a6f64e2b6f9977f2a1c715
wbham <wbham97@naver.com>
2023-08-17 18:06:17 +0900

    [Fix] MainActivity 터치 이벤트
    - EditText에 포커스가 맞춰져 있는 상태에서 키보드 제외 아무곳이나 클릭하면 키보드 내려감

--- core/presentation/src/main/kotlin/org/cazait/presentation/ui/MainActivity.kt
+++ core/presentation/src/main/kotlin/org/cazait/presentation/ui/MainActivity.kt
@@ -1,6 +1,10 @@
 package org.cazait.presentation.ui
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,5 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val focusView: View? = currentFocus
+        if (focusView != null) {
+            val rect = Rect()
+            focusView.getGlobalVisibleRect(rect)
+            val x = ev.x.toInt()
+            val y = ev.y.toInt()
+            if (!rect.contains(x, y)) {
+                val imm: InputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(focusView.windowToken, 0)
+                focusView.clearFocus()
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
